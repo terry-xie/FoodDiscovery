cd8dc47488be01d8af812b005fae3bb0c345e672
terry-xie <zkrew911@gmail.com>
2019-09-24 22:53:05 -0700

    Connect to Redis and update route

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const user = require("./routes/user.js");
-const session = require("./routes/session.js");
+const redis = require("redis");
+/*
+    TODO: Move initialization steps to own modules
+*/
+
+const client = redis.createClient();
+client.on("connect", () => {
+    console.log("Connected to Redis");
+});
+
+client.on("error", (err) => {
+    console.log(`Something went wrong connecting to Redis - ${err}`);
+});
 
 mongoose.connect("mongodb://localhost/authorization", {useNewUrlParser: true})
     .then(() => console.log("Connected to mongodb..."))
     .catch(() => console.log("Error connecting to mongodb..."));
 
 app.use(express.json());
-app.use("/api/session", session);
-app.use("/api/user", user);
+app.use("/api", require("./routes"));
 app.use((err, req, res, next) => {
 	console.log(err);
 	return res.status(500).send({ Error: "Internal Server Error" });
