e3d76c66318ca1f817a722323c5c9485b2c2dd7f
Terry Xie <terryxie@Terrys-MacBook-Pro.local>
2019-09-25 14:43:00 -0700

    Fix format

--- test/token.test.js
+++ test/token.test.js
@@ -2,48 +2,53 @@ const chai = require("chai");
 const chaiAsPromised = require("chai-as-promised");
 const sinon = require("sinon");
 const jwt = require("jsonwebtoken");
+const token = require("../security/token.js");
+
 chai.use(chaiAsPromised);
-const expect = chai.expect; 
+const { expect } = chai;
 
 describe("token Tests", () => {
-	describe("verifyToken()", () => { 
-		it("Should resolve with payload value when verify is successful.", () => {
-			sinon.stub(jwt, "verify").callsArgWith(3, null, "some payload");
-			let token = require("../security/token.js");
+  describe("verifyToken()", () => {
+    it("Should resolve with payload value when verify is successful.", () => {
+      sinon.stub(jwt, "verify").callsArgWith(3, null, "some payload");
 
-			expect(token.verifyToken("some token", "some private key", "some options")).to.eventually.be.equal("some payload");
+      expect(
+        token.verifyToken("some token", "some private key", "some options")
+      ).to.eventually.be.equal("some payload");
 
-			jwt.verify.restore();
-		});
+      jwt.verify.restore();
+    });
 
-		it("Should reject with error when verify fails.", () => {
-			sinon.stub(jwt, "verify").callsArgWith(3, "some error", null);
-			let token = require("../security/token.js");
+    it("Should reject with error when verify fails.", () => {
+      sinon.stub(jwt, "verify").callsArgWith(3, "some error", null);
 
-			expect(token.verifyToken("some token", "some private key", "some options")).to.eventually.be.rejectedWith("some error");
+      expect(
+        token.verifyToken("some token", "some private key", "some options")
+      ).to.eventually.be.rejectedWith("some error");
 
-			jwt.verify.restore();
-		});
-	});
+      jwt.verify.restore();
+    });
+  });
 
-	describe("generateToken()", () => {
-		it("Should resolve with token value when sign is successful.", () => {
-			sinon.stub(jwt, "sign").callsArgWith(3, null, "some token");
-			let token = require("../security/token.js");
+  describe("generateToken()", () => {
+    it("Should resolve with token value when sign is successful.", () => {
+      sinon.stub(jwt, "sign").callsArgWith(3, null, "some token");
 
-			expect(token.generateToken("some payload", "some private key", "some options")).to.eventually.be.equal("some token");
+      expect(
+        token.generateToken("some payload", "some private key", "some options")
+      ).to.eventually.be.equal("some token");
 
-			jwt.sign.restore();
-		});
+      jwt.sign.restore();
+    });
 
-		it("Should reject with error when sign fails.", () => {
-			sinon.stub(jwt, "sign").callsArgWith(3, "some error", null);
-			let token = require("../security/token.js");
+    it("Should reject with error when sign fails.", () => {
+      sinon.stub(jwt, "sign").callsArgWith(3, "some error", null);
 
-			expect(token.generateToken("some payload", "some private key", "some options")).to.eventually.be.rejectedWith("some error");
+      expect(
+        token.generateToken("some payload", "some private key", "some options")
+      ).to.eventually.be.rejectedWith("some error");
 
-			jwt.sign.restore();
-		});
-
-	});
+      jwt.sign.restore();
+    });
+  });
 });
