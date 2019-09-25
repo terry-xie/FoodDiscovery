c9da0f2566d26ed6bc4294282c53f8f0017fbdd4
Terry Xie <terryxie@Terrys-MacBook-Pro.local>
2019-09-25 14:41:59 -0700

    Format and fix tests

--- test/sessionController.test.js
+++ test/sessionController.test.js
@@ -1,137 +1,188 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable func-names */
 const chai = require("chai");
 const chaiAsPromised = require("chai-as-promised");
 const sinon = require("sinon");
 const httpMocks = require("node-mocks-http");
+
 chai.use(chaiAsPromised);
-const expect = chai.expect;
-const proxyquire = require("proxyquire").noCallThru();	//noCallThru speeds up test by not loading all objects from dependencies
+const { expect } = chai;
+const proxyquire = require("proxyquire").noCallThru(); // noCallThru speeds up test by not loading all objects from dependencies
 
-describe("sessionController Tests", function(){
-	let mockHttpRequest;
-	let mockHttpResponse;
-	let userStub;
-	let compareStub;
-	let tokenStub;
+describe("sessionController Tests", function() {
+  let mockHttpRequest;
+  let mockHttpResponse;
+  let userStub;
+  let compareStub;
+  let tokenStub;
 
-	describe("createSession()", function(){
-		beforeEach(function(){
-			mockHttpRequest = httpMocks.createRequest();
-			mockHttpResponse = httpMocks.createResponse();
+  describe("createSession()", function() {
+    beforeEach(function() {
+      mockHttpRequest = httpMocks.createRequest();
+      mockHttpResponse = httpMocks.createResponse();
 
-			userStub = {};
-			compareStub = {};
-			tokenStub = {}
-		});
+      userStub = {};
+      compareStub = {};
+      tokenStub = {};
+    });
 
-		it("Should return HTTP 400 and error when user is not found.", async function(){
-			userStub.findOne = sinon.stub().returns({
-				exec: sinon.stub().resolves(null)
-			});
-			compareStub.compare = sinon.stub();
-			tokenStub.generateToken = sinon.stub();
-			let sessionController = proxyquire("../controllers/sessionController.js", {
-				"../models/user.js": userStub,
-				"../security/encryptdecrypt.js": compareStub,
-				"../security/token.js": tokenStub
-			});
+    it("Should return HTTP 400 and error when user is not found.", async function() {
+      userStub.findOne = sinon.stub().returns({
+        exec: sinon.stub().resolves(null)
+      });
+      compareStub.compare = sinon.stub();
+      tokenStub.generateToken = sinon.stub();
+      const sessionController = proxyquire(
+        "../controllers/sessionController.js",
+        {
+          "../models/user.js": userStub,
+          "../security/encryptdecrypt.js": compareStub,
+          "../security/token.js": tokenStub
+        }
+      );
 
-			await sessionController.createSession(mockHttpRequest, mockHttpResponse, null);
-			let data = mockHttpResponse._getData();
+      await sessionController.createSession(
+        mockHttpRequest,
+        mockHttpResponse,
+        null
+      );
+      const data = mockHttpResponse._getData();
 
-			expect(mockHttpResponse.statusCode).to.equal(400);
-			expect(data.Error).to.exist;
+      expect(mockHttpResponse.statusCode).to.equal(400);
+      expect(data.Error).to.exist;
+    });
 
-		});
-		it("Should call next() once when user retrieval fails.", async function() {
-			userStub.findOne = sinon.stub().returns({
-				exec: sinon.stub().rejects({})
-			});
-			compareStub.compare = sinon.stub();
-			tokenStub.generateToken = sinon.stub();
-			let sessionController = proxyquire("../controllers/sessionController.js", {
-				"../models/user.js": userStub,
-				"../security/encryptdecrypt.js": compareStub,
-				"../security/token.js": tokenStub
-			});
-			let nextSpy = sinon.spy();
+    it("Should call next() once when user retrieval fails.", async function() {
+      userStub.findOne = sinon.stub().rejects({});
+      compareStub.compare = sinon.stub();
+      tokenStub.generateToken = sinon.stub();
+      const sessionController = proxyquire(
+        "../controllers/sessionController.js",
+        {
+          "../models/user.js": userStub,
+          "../security/encryptdecrypt.js": compareStub,
+          "../security/token.js": tokenStub
+        }
+      );
+      const nextSpy = sinon.spy();
 
-			await sessionController.createSession(mockHttpRequest, mockHttpResponse, nextSpy);
+      await sessionController.createSession(
+        mockHttpRequest,
+        mockHttpResponse,
+        nextSpy
+      );
 
-			expect(nextSpy.calledOnce).to.equal(true);
-		});
-		it("Should return HTTP 400 and error when password does not match.", async function(){ 
-			userStub.findOne = sinon.stub().returns({
-				exec: sinon.stub().resolves({  username: "someUserName", password: "somePassword" })
-			});
-			compareStub.compare = sinon.stub().resolves(false);
-			tokenStub.generateToken = sinon.stub();
-			let sessionController = proxyquire("../controllers/sessionController.js", {
-				"../models/user.js": userStub,
-				"../security/encryptdecrypt.js": compareStub,
-				"../security/token.js": tokenStub
-			});
+      expect(nextSpy.calledOnce).to.equal(true);
+    });
 
-			await sessionController.createSession(mockHttpRequest, mockHttpResponse, null);
-			let data = mockHttpResponse._getData();
+    it("Should return HTTP 400 and error when password does not match.", async function() {
+      userStub.findOne = sinon.stub().returns({
+        exec: sinon
+          .stub()
+          .resolves({ username: "someUserName", password: "somePassword" })
+      });
+      compareStub.compare = sinon.stub().resolves(false);
+      tokenStub.generateToken = sinon.stub();
+      const sessionController = proxyquire(
+        "../controllers/sessionController.js",
+        {
+          "../models/user.js": userStub,
+          "../security/encryptdecrypt.js": compareStub,
+          "../security/token.js": tokenStub
+        }
+      );
 
-			expect(mockHttpResponse.statusCode).to.equal(400);
-			expect(data.Error).to.exist;
-		});
-		it("Should call next() once when compare fails.", async function(){ 
-			userStub.findOne = sinon.stub().returns({
-				exec: sinon.stub().resolves({  username: "someUserName", password: "somePassword" })
-			});
-			compareStub.compare = sinon.stub().rejects({});
-			tokenStub.generateToken = sinon.stub();
-			let sessionController = proxyquire("../controllers/sessionController.js", {
-				"../models/user.js": userStub,
-				"../security/encryptdecrypt.js": compareStub,
-				"../security/token.js": tokenStub
-			});
-			let nextSpy = sinon.spy();
+      await sessionController.createSession(
+        mockHttpRequest,
+        mockHttpResponse,
+        null
+      );
+      const data = mockHttpResponse._getData();
 
-			await sessionController.createSession(mockHttpRequest, mockHttpResponse, nextSpy);
+      expect(mockHttpResponse.statusCode).to.equal(400);
+      expect(data.Error).to.exist;
+    });
 
-			expect(nextSpy.calledOnce).to.equal(true);
-		});
-		it("Should return HTTP 201 and a token on success.", async function() {
-			userStub.findOne = sinon.stub().returns({
-				exec: sinon.stub().resolves({  username: "someUserName", password: "somePassword" })
-			});
-			compareStub.compare = sinon.stub().resolves(true);
-			tokenStub.generateToken = sinon.stub().resolves("a token");
-			let sessionController = proxyquire("../controllers/sessionController.js", {
-				"../models/user.js": userStub,
-				"../security/encryptdecrypt.js": compareStub,
-				"../security/token.js": tokenStub
-			});
-			
-			await sessionController.createSession(mockHttpRequest,mockHttpResponse,null);
-			let data = mockHttpResponse._getData();
-			
-			expect(mockHttpResponse.statusCode).to.equal(201);
-			expect(data.Token).to.equal("a token");
-			
-		});
-		it("Should call next() once when token generation fails.", async function(){
-			userStub.findOne = sinon.stub().returns({
-				exec: sinon.stub().resolves({  username: "someUserName", password: "somePassword" })
-			});
-			compareStub.compare = sinon.stub().resolves(true);
-			tokenStub.generateToken = sinon.stub().rejects({});
-			let sessionController = proxyquire("../controllers/sessionController.js", {
-				"../models/user.js": userStub,
-				"../security/encryptdecrypt.js": compareStub,
-				"../security/token.js": tokenStub
-			});
-			let nextSpy = sinon.spy();
+    it("Should call next() once when compare fails.", async function() {
+      userStub.findOne = sinon.stub().returns({
+        exec: sinon
+          .stub()
+          .resolves({ username: "someUserName", password: "somePassword" })
+      });
+      compareStub.compare = sinon.stub().rejects({});
+      tokenStub.generateToken = sinon.stub();
+      const sessionController = proxyquire(
+        "../controllers/sessionController.js",
+        {
+          "../models/user.js": userStub,
+          "../security/encryptdecrypt.js": compareStub,
+          "../security/token.js": tokenStub
+        }
+      );
+      const nextSpy = sinon.spy();
 
-			await sessionController.createSession(mockHttpRequest, mockHttpResponse, nextSpy);
+      await sessionController.createSession(
+        mockHttpRequest,
+        mockHttpResponse,
+        nextSpy
+      );
 
-			expect(nextSpy.calledOnce).to.equal(true);
-		});
+      expect(nextSpy.calledOnce).to.equal(true);
+    });
 
-	});
+    it("Should return HTTP 201 and a token on success.", async function() {
+      userStub.findOne = sinon.stub().returns({
+        exec: sinon
+          .stub()
+          .resolves({ username: "someUserName", password: "somePassword" })
+      });
+      compareStub.compare = sinon.stub().resolves(true);
+      tokenStub.generateToken = sinon.stub().resolves("a token");
+      const sessionController = proxyquire(
+        "../controllers/sessionController.js",
+        {
+          "../models/user.js": userStub,
+          "../security/encryptdecrypt.js": compareStub,
+          "../security/token.js": tokenStub
+        }
+      );
 
+      await sessionController.createSession(
+        mockHttpRequest,
+        mockHttpResponse,
+        null
+      );
+      const data = mockHttpResponse._getData();
 
+      expect(mockHttpResponse.statusCode).to.equal(201);
+      expect(data.Token).to.equal("a token");
+    });
+
+    it("Should call next() once when token generation fails.", async function() {
+      userStub.findOne = sinon.stub().returns({
+        exec: sinon
+          .stub()
+          .resolves({ username: "someUserName", password: "somePassword" })
+      });
+      compareStub.compare = sinon.stub().resolves(true);
+      tokenStub.generateToken = sinon.stub().rejects({});
+      const sessionController = proxyquire(
+        "../controllers/sessionController.js",
+        {
+          "../models/user.js": userStub,
+          "../security/encryptdecrypt.js": compareStub,
+          "../security/token.js": tokenStub
+        }
+      );
+      const nextSpy = sinon.spy();
+
+      await sessionController.createSession(
+        mockHttpRequest,
+        mockHttpResponse,
+        nextSpy
+      );
+
+      expect(nextSpy.calledOnce).to.equal(true);
+    });
+  });
 });
