82ffb0f9a55086052df50cb1cbbf078c35d9adff
terry-xie <zkrew911@gmail.com>
2019-09-24 22:56:02 -0700

    Update to resolve payload

--- security/token.js
+++ security/token.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 
 function generateJWT(payload, privateKey, options) {
 	return new Promise((resolve, reject) => {
-		jwt.sign(payload, privateKey, options || null, (err, token) => {
+		jwt.sign(payload, privateKey, options, (err, token) => {
 			if(err)
 				return reject(err);
 			resolve(token);
@@ -13,11 +13,13 @@ function generateJWT(payload, privateKey, options) {
 
 function verifyJWT(token, privateKey, options) {
 	return new Promise((resolve, reject) => {
-		jwt.verify(token, privateKey, options || null, (err, payload) => {
-			resolve(!err);
+		jwt.verify(token, privateKey, options, (err, payload) => {
+			if(err)
+				return reject(err)
+			resolve(payload);
 		});
 	});
-}
+};
 
 module.exports.generateToken = generateJWT;
 module.exports.verifyToken = verifyJWT;
