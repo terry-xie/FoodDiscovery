bf9d0ce65bfa29d03091c13997cd6a9ddeba1135
Terry Xie <terryxie@Terrys-MacBook-Pro.local>
2019-09-30 22:46:28 -0700

    Update filename to camel case

---
+++ middleware/tokenValidation.js
@@ -0,0 +1,21 @@
+const { verifyToken } = require("../security/token.js");
+
+async function verifyRequestToken(req, res, next) {
+  const authorizationHeader = req.headers.authorization;
+  if (!authorizationHeader)
+    return res.status(400).send({ Error: "Missing Authorization Header" });
+  if (!authorizationHeader.startsWith("Bearer "))
+    return res.status(400).send({ Error: "Invalid Authorization Header" });
+  const token = authorizationHeader.slice(7);
+  let payload;
+  try {
+    payload = await verifyToken(token, process.env.APP_PRIVATEKEY);
+  } catch (err) {
+    console.log("Error verifying token");
+    return res.status(400).send({ Error: "Invalid Token" });
+  }
+  res.locals.userId = payload.sub;
+  return next();
+}
+
+module.exports = verifyRequestToken;
