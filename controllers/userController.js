aff7f717c8f0a13b86a731a1ff346286efafa4c2
Terry Xie <terryxie@Terrys-MacBook-Pro.local>
2019-11-06 16:59:34 -0800

    Update response to be in JSON format

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,13 +4,13 @@ const { hash } = require("../security/encryptdecrypt.js");
 async function createUser(req, res, next) {
   try {
     let user = await User.findOne({ username: req.body.username });
-    if (user) return res.status(400).send("User Exists");
+    if (user) return res.status(400).json({ error: "user exists" });
     const hashedPassword = await hash(req.body.password, 10);
     user = await User.create({
       username: req.body.username,
       password: hashedPassword
     });
-    return res.status(201).send({ _id: user._id, username: user.username });
+    return res.status(201).json({ _id: user._id, username: user.username });
   } catch (err) {
     return next(err);
   }
@@ -25,7 +25,7 @@ async function getUser(req, res, next) {
         username: user.username
       };
     });
-    return res.status(200).send({ users });
+    return res.status(200).json({ users });
   } catch (err) {
     return next(err);
   }
@@ -34,7 +34,7 @@ async function getUser(req, res, next) {
 async function getUserById(req, res, next) {
   try {
     const user = await User.findById(req.params.userId);
-    return res.status(200).send({ _id: user._id, username: user.username });
+    return res.status(200).json({ _id: user._id, username: user.username });
   } catch (err) {
     return next(err);
   }
@@ -49,7 +49,7 @@ async function updateUserById(req, res, next) {
       },
       { new: true }
     );
-    return res.status(200).send({ _id: user._id, username: user.username });
+    return res.status(200).json({ _id: user._id, username: user.username });
   } catch (err) {
     return next(err);
   }
